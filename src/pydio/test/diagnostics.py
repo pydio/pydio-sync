f0fb95e4e7ffaaab6d13d75775062d7782490065
cdujeu <charles@ajaxplorer.info>
2014-06-03 17:09:43 +0200

    Fix diagnostic class after zmq hardcore removal

--- src/pydio/test/diagnostics.py
+++ src/pydio/test/diagnostics.py
@@ -7,53 +7,18 @@ from pydio.sdk.remote import PydioSdk
 
 
 class PydioDiagnostics():
-    # zmq_address = 'tcp://127.0.0.1:5678'
-    #
-    # def __init__(self, url, basepath, ws_id, user_id):
-    #     self.url = url
-    #     self.basepath = basepath
-    #     self.ws_id = ws_id
-    #     self.user_id = user_id
-    #     self.status = 0
-    #     self.status_message = None
-    #
-    # def run(self):
-    #     self.run_zmq_smoke_test()
-    #     self.run_ping_server_test()
-    #     return self.status
-    #
-    # @classmethod
-    # def run_zmq_smoke_test(cls):
-    #     logging.info('Start ZMQ test')
-    #     receive_process = multiprocessing.Process(
-    #         target=cls._receive_message_from_zmq_and_send_reply)
-    #     receive_process.start()
-    #     cls._send_message_to_zmq_and_wait_for_reply()
-    #     receive_process.join()
-    #
-    # @classmethod
-    # def _send_message_to_zmq_and_wait_for_reply(cls):
-    #     context = zmq.Context()
-    #     sock = context.socket(zmq.REQ)
-    #     sock.bind(cls.zmq_address)
-    #
-    #     message = '--- Test message send to zmq ---'
-    #     logging.info('Sending request: %s' % message)
-    #     sock.send_unicode(message)
-    #     response = sock.recv_unicode()
-    #     logging.debug('Receiving response: %s' % response)
-    #
-    # @classmethod
-    # def _receive_message_from_zmq_and_send_reply(cls):
-    #     context = zmq.Context()
-    #     sock = context.socket(zmq.REP)
-    #     sock.connect(cls.zmq_address)
-    #
-    #     request = sock.recv_unicode()
-    #     logging.info('Receiving request: %s' % request)
-    #     message = 'Test response sent to zmq'
-    #     logging.debug('Sending response: %s' % message)
-    #     sock.send_unicode(message)
+
+    def __init__(self, url, basepath, ws_id, user_id):
+        self.url = url
+        self.basepath = basepath
+        self.ws_id = ws_id
+        self.user_id = user_id
+        self.status = 0
+        self.status_message = None
+
+    def run(self):
+        self.run_ping_server_test()
+        return self.status
 
     def run_ping_server_test(self):
         logging.info('---- Server ping test ---')
@@ -68,7 +33,7 @@ class PydioDiagnostics():
             return
 
         pydio_sdk = PydioSdk(self.url, self.basepath, self.ws_id or '', self.user_id)
-        success = pydio_sdk.stat('/')
+        success = pydio_sdk.stat(unicode('/', 'utf-8'))
         logging.info('Server ping test: %s' % ('success' if success else 'failure'))
         if not success:
             self.status = -1
