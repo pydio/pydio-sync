5465d62fdc44cc97e1cd6a5de30684fe3a09401c
pydio <jabar@ajaxplorer.info>
2014-07-25 17:54:44 +0200

    added QuotaException and FilePermissionException which are specific pydioSdkDefaultException

--- src/pydio/sdk/exceptions.py
+++ src/pydio/sdk/exceptions.py
@@ -30,21 +30,31 @@ class PydioSdkException(ProcessException):
     def __init__(self, operation, path, detail):
         super(PydioSdkException, self).__init__('sdk operation', operation, path, detail)
 
-
 class SystemSdkException(ProcessException):
     def __init__(self, operation, path, detail):
         super(SystemSdkException, self).__init__('system operation', operation, path, detail)
-
 
 class PydioSdkBasicAuthException(Exception):
     def __init__(self, type):
         super(PydioSdkBasicAuthException, self).__init__('Http-Basic authentication failed, wrong credentials?')
 
-
 class PydioSdkTokenAuthException(Exception):
     def __init__(self, type):
         super(PydioSdkTokenAuthException, self).__init__('Token-based authentication failed, reload credentials?')
 
+class PydioSdkDefaultException(Exception):
+    def __init__(self, message):
+        super(PydioSdkDefaultException, self).__init__(message)
+
+class PydioSdkQuotaException(PydioSdkDefaultException):
+    def __init__(self, file_name, file_size, usage, total):
+        super(PydioSdkQuotaException, self).__init__('[Quota limit reached] - You are using'+' {0:.2f}'.format(float(usage)/(1024*1024))+' of'+' {0:.2f}'.format(float(total)/(1024*1024))+' Mo, '
+                                                    'cannot upload '+file_name+'('+"{0:.2f}".format(float(file_size)/(1024*1024))+' Mo)')
+        self.code = 507
+class PydioSdkPermissionException(PydioSdkDefaultException):
+    def __init__(self, message):
+        super(PydioSdkDefaultException, self).__init__('[File permission] - '+message)
+        self.code = 412
 
 class InterruptException(Exception):
     def __init__(self):
