596677cba59a01ded16c3091eb2091f22197d853
sagar UDAY KUMAR <sagaru@users.noreply.github.com>
2015-10-20 19:28:36 +0200

    Adding a resource UrlManager
    Adding a resource UrlManager - In presence of proxy sends the request via proxy

--- src/pydio/ui/web_api.py
+++ src/pydio/ui/web_api.py
@@ -106,6 +106,7 @@ class PydioApi(Api):
         self.add_resource(ConflictsManager, '/jobs/<string:job_id>/conflicts', '/jobs/conflicts')
         self.add_resource(CmdManager, '/cmd/<string:cmd>/<string:job_id>', '/cmd/<string:cmd>')
         self.add_resource(ProxyManager, '/proxy')
+        self.add_resource(UrlManager, '/url/<path:complete_url>')
         self.app.add_url_rule('/res/i18n.js', 'i18n', self.serve_i18n_file)
         self.app.add_url_rule('/res/config.js', 'config', self.server_js_config)
         self.app.add_url_rule('/res/dynamic.css', 'dynamic_css', self.serve_dynamic_css)
@@ -590,3 +591,10 @@ class ProxyManager(Resource):
         # write the content into local proxy.json file
         response = ConfigManager.Instance().set_user_proxy(json_req, check_proxy_flag=proxy_flag)
         return response
+
+class UrlManager(Resource):
+    @authDB.requires_auth
+    @pydio_profile
+    def get(self, complete_url):
+        resp = requests.get(complete_url, stream=False, proxies=ConfigManager.Instance().get_defined_proxies())
+        return json.loads(resp.content)
