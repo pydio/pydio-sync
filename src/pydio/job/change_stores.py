d9384f1f239270bd215d02d902297885e9d7faf3
Thomas <thomas@pydio.com>
2016-07-22 14:03:11 +0200

    Hotfix status

--- src/pydio/job/change_stores.py
+++ src/pydio/job/change_stores.py
@@ -209,7 +209,7 @@ class SqliteChangeStore():
                         if i.status == "SUCCESS" or (hasattr(i, "error") and hasattr(i.error, "code") and i.error.code == 1404):  # file download impossible -> Assume deleted from server
                             self.conn.execute('DELETE FROM ajxp_changes WHERE row_id=?', (i.change['row_id'],))
                             #logging.info("DELETE CHANGE %s" % i.change)
-                            if i.change is not None:
+                            if i.change is not None and hasattr(i.change, 'status'):
                                 i.change.status = "FAILED"
                                 self.change_history.insert_change(i)
                         else:
