654da7174acca594821e49a652a231cb6265e0bf
Louis Thibault <louis@pydio.com>
2017-04-24 13:42:47 +0200

    simplified canary test

--- test/test_canary.py
+++ test/test_canary.py
@@ -5,4 +5,3 @@ class CanaryTest(unittest.TestCase):
     def test_canary(self):
         """This test should never fail."""
         self.assertTrue(True, "this should never happen.")
-        self.assertRaises(ZeroDivisionError, (lambda: 1 / 0))
